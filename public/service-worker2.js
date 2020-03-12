b7e94c2df5714324618b813d2e641d5902dd3af3
Roman Gorisek <roman@code-lab.si>
2020-03-12 08:35:17 +0100

    service workers again

--- public/service-worker2.js
+++ public/service-worker2.js
@@ -35,15 +35,5 @@ self.addEventListener('activate', e => {
 })
 
 self.addEventListener('fetch', function (event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function (response) {
-        // Cache hit - return response
-        if (response) {
-          return response
-        }
-        return fetch(event.request)
-      },
-      ),
-  )
+  event.respondWith(fetch(event.request))
 })
