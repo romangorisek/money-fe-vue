0e9f67038f59f08da6e47da6489d96acb82eb78f
Roman Gorisek <roman@code-lab.si>
2020-04-23 10:27:20 +0200

    new settings icon

--- src/components/TheNavBar.vue
+++ src/components/TheNavBar.vue
@@ -96,7 +96,7 @@ export default {
           route: 'Business',
         }, {
           title: 'Nastavitve',
-          icon: 'mdi-settings-outline',
+          icon: 'fas fa-tools',
           route: 'Settings',
         },
       ],
